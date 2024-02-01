83a22615136ae3617367c680e68f4b295e64b3eb
Hariprasad Penta <hariprasadpenta@Hariprasads-MacBook-Pro.local>
2024-02-01 15:11:25 +0530

    Initial Commit for Real Estate project using flutter

---
+++ lib/pages/chat_main/chat_main_widget.dart
@@ -0,0 +1,79 @@
+import '/auth/firebase_auth/auth_util.dart';
+import '/backend/backend.dart';
+import '/flutter_flow/chat/index.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'chat_main_model.dart';
+export 'chat_main_model.dart';
+
+class ChatMainWidget extends StatefulWidget {
+  const ChatMainWidget({super.key});
+
+  @override
+  State<ChatMainWidget> createState() => _ChatMainWidgetState();
+}
+
+class _ChatMainWidgetState extends State<ChatMainWidget> {
+  late ChatMainModel _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => ChatMainModel());
+  }
+
+  @override
+  void dispose() {
+    _model.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (isiOS) {
+      SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(
+          statusBarBrightness: Theme.of(context).brightness,
+          systemStatusBarContrastEnforced: true,
+        ),
+      );
+    }
+
+    context.watch<FFAppState>();
+
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+      appBar: AppBar(
+        backgroundColor: FlutterFlowTheme.of(context).dark600,
+        automaticallyImplyLeading: false,
+        title: Text(
+          'All Chats',
+          style: FlutterFlowTheme.of(context).headlineMedium.override(
+                fontFamily: 'Urbanist',
+                color: FlutterFlowTheme.of(context).tertiary,
+                fontWeight: FontWeight.bold,
+              ),
+        ),
+        actions: [],
+        centerTitle: false,
+        elevation: 2.0,
+      ),
+      body: const SafeArea(
+        top: true,
+        child: Padding(
+          padding: EdgeInsetsDirectional.fromSTEB(0.0, 2.0, 0.0, 0.0),
+          child: Center(child: Text("No Records found")),
+        ),
+      ),
+    );
+  }
+}
